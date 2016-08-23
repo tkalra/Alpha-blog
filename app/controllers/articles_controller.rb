51ab3ca0039f507e450b6bd70293e617ebfa3f60
Tushar kalra <tkalraoct31@gmail.com>
2016-08-23 22:01:35 +0000

    added debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,9 +26,11 @@ class ArticlesController < ApplicationController
   end
 
   def create
+
      @article = Article.new(article_params)
-    if @article.save
-      flash[:success] = "Article was successfully created"
+    @article.user = User.first
+    if @article.saves
+      flash[:success] = "Article was successfully created" 
       redirect_to article_path(@article)
      else
       render 'new'
