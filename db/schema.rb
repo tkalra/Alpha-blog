9a8107811baf5e0e970a0cabab246d468a221571
Tushar kalra <tkalraoct31@gmail.com>
2016-08-24 06:11:19 +0000

    Added secure password for user authenticaion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819212543) do
+ActiveRecord::Schema.define(version: 20160824060426) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160819212543) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
