6c893545a8b535aff553a7dbf4fe826d0063a4a7
tkalra <tkalraoct31@gmail.com>
2016-09-01 14:19:17 -0700

    added pagination to all the listing views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit,:update,:show,:destroy]
 
  def index
-   @articles = Article.all
+   @articles = Article.paginate(page: params[:page], per_page: 8)
 
  end
 
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
 
      @article = Article.new(article_params)
     @article.user = User.first
-    if @article.saves
+    if @article.save
       flash[:success] = "Article was successfully created"
 
       redirect_to article_path(@article)
